ceac057ba5eff7b9fea2d6ef0d670879d8e674ab
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2024-08-31 21:53:30 +0800

    Initial commit

---
+++ Say No More/Say_No_MoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Say_No_MoreApp.swift
+//  Say No More
+//
+//  Created by Kai Quan Tay on 31/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Say_No_MoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
