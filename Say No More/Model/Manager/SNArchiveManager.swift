299502b3d319ec7d1801422007c3cd791f55a4e0
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2024-09-03 21:01:49 +0800

    Add home view

--- Say No More/Model/Manager/SNArchiveManager.swift
+++ Say No More/Model/Manager/SNArchiveManager.swift
@@ -7,8 +7,17 @@
 
 import Foundation
 
+protocol ArchiveManager: Observation.Observable {
+    static var shared: Self { get }
+
+    func getGameDates() -> [Date]
+    func readGame(for date: Date) -> SNGame?
+    func saveGame(_ game: SNGame, for date: Date)
+    func removeGame(for date: Date)
+}
+
 @Observable
-class SNArchiveManager {
+final class SNArchiveManager: ArchiveManager {
     var gameDates: [Date] = []
     private var gamesArchive: [Date: SNGame] = [:]
 
@@ -23,6 +32,10 @@ class SNArchiveManager {
             .init(gameDates: [])
         }
     }()
+
+    func getGameDates() -> [Date] {
+        gameDates
+    }
 
     func readGame(for date: Date) -> SNGame? {
         if let game = gamesArchive[date] {
@@ -55,5 +68,49 @@ class SNArchiveManager {
 
     private func fileNameForGameWith(date: Date) -> String {
         "game-\(date.timeIntervalSince1970).json"
+    }
+}
+
+final class SNArchiveMockManager: ArchiveManager {
+    var gamesArchive: [Date: SNGame] = [:]
+
+    private init(gamesArchive: [Date: SNGame]) {
+        self.gamesArchive = gamesArchive
+    }
+
+    static var shared: SNArchiveMockManager = .init(
+        gamesArchive: {
+            var archive: [Date: SNGame] = [:]
+            for interval in 1..<10 {
+                archive[.now.addingTimeInterval(Double(interval) * 60)] = .init(
+                    turns: [
+                        .init(team: .team1, won: 10, forfeit: 1),
+                        .init(team: .team2, won: 4, forfeit: 0),
+                        .init(team: .team1, won: 5, forfeit: 1),
+                        .init(team: .team2, won: 3, forfeit: 1),
+                        .init(team: .team1, won: 1, forfeit: 0),
+                        .init(team: .team2, won: 5, forfeit: 2)
+                    ]
+                )
+            }
+            return archive
+        }()
+    )
+
+    func getGameDates() -> [Date] {
+        gamesArchive.keys.sorted()
+    }
+
+    func readGame(for date: Date) -> SNGame? {
+        print("READING GAME: \(date)")
+        return gamesArchive[date]
+    }
+
+    func saveGame(_ game: SNGame, for date: Date) {
+        gamesArchive[date] = game
+    }
+
+    func removeGame(for date: Date) {
+        gamesArchive.removeValue(forKey: date)
     }
 }
---
+++ Say No More/UI/Pages/HomeView.swift
@@ -0,0 +1,74 @@
+//
+//  HomeView.swift
+//  Say No More
+//
+//  Created by Kai Quan Tay on 2/9/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State var archive: any ArchiveManager
+
+    @State var showGamePlay: Bool = false
+
+    var body: some View {
+        GeometryReader { geom in
+            List {
+                Section {
+                    VStack(alignment: .center) {
+                        Text("Say No More")
+                            .font(.largeTitle)
+                            .bold()
+
+                        Text("🤐")
+                            .font(.system(size: 100))
+                    }
+                    .frame(maxWidth: .infinity)
+                    .listRowBackground(Color.clear)
+                    .listRowSeparator(.hidden)
+                }
+
+                Section {
+                    Button {
+                        showGamePlay = true
+                    } label: {
+                        Text("Play!")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 40)
+                            .multilineTextAlignment(.center)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                }
+
+                Section("Past Games") {
+                    LazyVGrid(
+                        columns: .init(repeating: .init(), count: Int(geom.size.width / 150))
+                    ) {
+                        ForEach(archive.getGameDates(), id: \.self) { date in
+                            if let game = archive.readGame(for: date) {
+                                GameSummaryView(game: game, date: date)
+                                    .padding(10)
+                            }
+                        }
+                    }
+                    .padding(-5)
+                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                }
+            }
+            .scrollContentBackground(.hidden)
+            .background {
+                Color(uiColor: .systemBackground)
+            }
+        }
+        .fullScreenCover(isPresented: $showGamePlay) {
+            // TODO: use actual card provider
+            GameView(gameManager: .init(cardProvider: SNSequentialCardProvider()))
+        }
+    }
+}
+
+#Preview {
+    HomeView(archive: SNArchiveMockManager.shared)
+}
